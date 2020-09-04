b96ddf9b3bdec4c990de4e30245fac8a087d57c8
sdatta11 <69872880+sdatta11@users.noreply.github.com>
2020-09-04 16:02:15 -0500

    Create helloWorld.c

---
+++ helloWorld.c
@@ -0,0 +1,19 @@
+/**
+* Author: Shaunak Datta 
+* Date: 2020/09/02
+*
+* A simple hello world style program that prints my name
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv){
+
+  printf( "Shaunak Datta\n");
+  
+  return 0;
+  
+  
+  }
+  
